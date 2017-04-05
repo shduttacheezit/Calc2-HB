eabcbad00bf1035af7445788bb3d820e93aa93fb
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-05 16:14:21 -0700

    added squared and cubed functions

--- calculator.py
+++ calculator.py
@@ -16,4 +16,9 @@ elif tokens[0] == "-":
     print subtract(int(tokens[1]), int(tokens[2]))
 elif tokens[0] == "*":
     print multiply(int(tokens[1]), int(tokens[2]))
-    
+elif tokens[0] == "/":
+    print divide(int(tokens[1]), int(tokens[2]))
+elif tokens[0] == "square":
+    print square(int(tokens[1]))
+elif tokens[0] == "cube":
+    print cube(int(tokens[1]))
