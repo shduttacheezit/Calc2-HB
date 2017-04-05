73ff9388bd7f95bd0dd110362daeb38a4d038816
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-05 16:18:32 -0700

    added remainder function

--- calculator.py
+++ calculator.py
@@ -24,4 +24,5 @@ elif tokens[0] == "cube":
     print cube(int(tokens[1]))
 elif tokens[0] == "pow":
     print power(int(tokens[1]), int(tokens[2]))
-    
+elif tokens[0] == "mod":
+    print mod(int(tokens[1]), int(tokens[2]))
