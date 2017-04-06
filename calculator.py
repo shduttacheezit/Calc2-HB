cfa7f607ca9c33304c273ea816d92b8d3239be6f
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-05 17:30:53 -0700

    fixed the while loop

--- calculator.py
+++ calculator.py
@@ -5,29 +5,35 @@ calculator program yourself in this file.
 """
 
 from arithmetic import *
-import sys
-
 
 # Your code goes here
-input_string = raw_input(">")
-tokens = input_string.split(" ")
-if tokens[0] == "q":
-    sys.exit
-elif tokens[0] == "+":
-    print add(int(tokens[1]), int(tokens[2]))
-elif tokens[0] == "-":  
-    print subtract(int(tokens[1]), int(tokens[2]))
-elif tokens[0] == "*":
-    print multiply(int(tokens[1]), int(tokens[2]))
-elif tokens[0] == "/":
-    print divide(int(tokens[1]), int(tokens[2]))
-elif tokens[0] == "square":
-    print square(int(tokens[1]))
-elif tokens[0] == "cube":
-    print cube(int(tokens[1]))
-elif tokens[0] == "pow":
-    print power(int(tokens[1]), int(tokens[2]))
-elif tokens[0] == "mod":
-    print mod(int(tokens[1]), int(tokens[2]))
-else:
-    print "I don't understand"
+
+while True:
+    input_string = raw_input(">")
+    tokens = input_string.split(" ")
+    if tokens[0] == "q":
+        break
+    try:
+        i = len(tokens)
+        num1 = int(tokens[1])
+        num2 = int(tokens[-1])
+        if tokens[0] == "+":
+            print add(num1, num2)
+        elif tokens[0] == "-":
+            print subtract(num1, num2)
+        elif tokens[0] == "*":
+            print multiply(num1, num2)
+        elif tokens[0] == "/":
+            print divide(num1, float(num2))
+        elif tokens[0] == "square":
+            print square(num1)
+        elif tokens[0] == "cube":
+            print cube(num1)
+        elif tokens[0] == "pow":
+            print power(num1, num2)
+        elif tokens[0] == "mod":
+            print mod(num1, num2)
+        else:
+            print "I don't understand"
+    except:
+        print "I don't understand"
